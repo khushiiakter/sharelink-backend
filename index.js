897dda4c5a816bbc145d42d3a38bab95eafd4cf8
khushiiakter <rkjia1311@gmail.com>
2025-03-02 03:17:53 +0600

    added the userscollection on database

--- index.js
+++ index.js
@@ -28,12 +28,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("sharelinkDb").collection("users");
+
+    app.post("/users", async (req, res) => {
+        const { _id, email, name, photo } = req.body;
+        const existingUser = await usersCollection.findOne({ email });
+  
+        if (!existingUser) {
+          const result = await usersCollection.insertOne({
+            _id,
+            email,
+            name,
+            photo,
+            timestamp: new Date().toLocaleDateString(),
+          });
+          res.send(result);
+        } else{
+          res.send({ message: "User already exists" });
+        }
+      });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
